144b75a4f2326eaf9aa2bf98c5b8b1519337d28d
allahverdiyevabdulhasan <info.neyman.e.t@gmail.com>
2024-07-19 02:00:16 +0400

    send change

--- src/context/ChatContext.jsx
+++ src/context/ChatContext.jsx
@@ -11,8 +11,8 @@ export const ChatContext = createContext();
 
 export default function ChatProvider({ children }) {
     const {user} = useContext(AuthContext)
-    const CHAT_API = 'http://46.101.153.252:8000/api/chat/';
-    const SOCKET_URL = 'http://31.210.51.68:8300';
+    const CHAT_API = 'https://neymantech.online/api/chat/';
+    const SOCKET_URL = 'https://neymantech.site';
 
     let socketInstance = '';
     const USER_TOKKEN = localStorage.authTokens != undefined ? JSON.parse(localStorage.authTokens).access : false;
