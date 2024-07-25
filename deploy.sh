a43eb11bc57d2fad3c246debf152af8e664d0b16
allahverdiyevabdulhasan <info.neyman.e.t@gmail.com>
2024-07-25 17:08:01 +0400

    send

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ echo "Bulding app ..."
 npm run build
 
 echo "Deploying files to server..."
-scp -r dist/* bivisi@157.230.120.254:/var/www/bivisi/
+scp -r dist/* bivisi@159.65.115.9:/var/www/bivisi/
 echo "Done!"
