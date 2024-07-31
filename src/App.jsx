b3fa9d4cc74cb9f5da7cd3ee11ada92d01e0e2bb
sem1103 <sem.sem.geo.az1998@mail.ru>
2024-08-01 01:02:25 +0400

    theme mode system 40%

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { AuthProvider } from "./context/authContext";
 import AppRouter from "./router";
 import { VideoProvider } from "./context/VideoContext";
 import ChatProvider from "./context/ChatContext";
+import ThemeProvider from "./context/ThemeContext";
 
 function App() {
   return (
     <>
-      <AuthProvider>
-        <VideoProvider>
-          <ChatProvider>
-            <AppRouter />
-          </ChatProvider>
-        </VideoProvider>
-      </AuthProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <VideoProvider>
+            <ChatProvider>
+              <AppRouter />
+            </ChatProvider>
+          </VideoProvider>
+        </AuthProvider>
+      </ThemeProvider>
     </>
   );
 }
