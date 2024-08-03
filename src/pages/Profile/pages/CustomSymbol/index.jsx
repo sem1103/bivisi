d8d8e2a42147025a9a314251e80787d64fe4ef32
allahverdiyevabdulhasan <info.neyman.e.t@gmail.com>
2024-08-03 13:02:06 +0400

    send change and comment

--- src/pages/Profile/pages/CustomSymbol/index.jsx
+++ src/pages/Profile/pages/CustomSymbol/index.jsx
@@ -1,7 +1,4 @@
 import { components } from 'react-select';
-
-
-
 const CustomSingleValue = (props) => (
     <components.SingleValue {...props}>
    <svg style={{margin: '0 5px  0 0'}} width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
