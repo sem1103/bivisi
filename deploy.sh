663e900bf8b9fd104afed170ff181ac051d9b437
sem1103 <sem.sem.geo.az1998@mail.ru>
2024-08-05 12:22:27 +0400

    last version

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 # echo "Switching to branch master"
 # git checkout master
 
-# echo "Bulding app ..."
-# npm run build
+echo "Bulding app ..."
+npm run build
 
 echo "Deploying files to server..."
 scp -r dist/* bivisi@159.65.115.9:/var/www/bivisi/
