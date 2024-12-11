0a6a581685607b2e6dc4ceec6138bc4f7b2539d3
sem1103 <sem.sem.geo.az1998@mail.ru>
2024-12-11 18:29:21 +0400

    deploy

--- src/pages/Profile/pages/Favorites/index.jsx
+++ src/pages/Profile/pages/Favorites/index.jsx
@@ -138,14 +138,14 @@ const Favorites = () => {
               <h1>Favorites videos</h1>
               <div className="d-flex gap-3 flex-wrap align-items-center">
 
-              <button className="favorites_videos_filter stroke__change" onClick={() => setShowModal(true)}>
+              {/* <button className="favorites_videos_filter stroke__change" onClick={() => setShowModal(true)}>
               <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g id="Icon/Filter">
 <path id="Vector" d="M2.5 4.16667H8.33333M8.33333 4.16667C8.33333 5.08714 9.07953 5.83333 10 5.83333C10.9205 5.83333 11.6667 5.08714 11.6667 4.16667M8.33333 4.16667C8.33333 3.24619 9.07953 2.5 10 2.5C10.9205 2.5 11.6667 3.24619 11.6667 4.16667M2.5 10H10M15.8333 10H17.5M15.8333 10C15.8333 10.9205 15.0871 11.6667 14.1667 11.6667C13.2462 11.6667 12.5 10.9205 12.5 10C12.5 9.07953 13.2462 8.33333 14.1667 8.33333C15.0871 8.33333 15.8333 9.07953 15.8333 10ZM11.6667 4.16667H17.5M10 15.8333H17.5M2.5 15.8333H4.16667M4.16667 15.8333C4.16667 16.7538 4.91286 17.5 5.83333 17.5C6.75381 17.5 7.5 16.7538 7.5 15.8333C7.5 14.9129 6.75381 14.1667 5.83333 14.1667C4.91286 14.1667 4.16667 14.9129 4.16667 15.8333Z" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
 </g>
 </svg>
                 Filter
-            </button>
+            </button> */}
      {showModal&&<FilterModal setShowModal={setShowModal} applyFilter={applyFilter} />}
                 <div className="custom-select">
                   <SortProduct sortedProducts={favorites} setSortedProducts={setFavorites} />
